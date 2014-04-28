b43eb479182c0fc477063cfa8a8953cf92cb20da
Sander van Harmelen <svanharmelen@schubergphilis.com>
2014-04-28 19:43:12 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'sbp_cloudmonkey'
+maintainer       'Sander van Harmelen'
+maintainer_email 'svanharmelen@schubergphilis.com'
+license          'Apache 2.0'
+description      'Installs/Configures cloudmonkey'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports         'redhat'
+supports         'centos'
